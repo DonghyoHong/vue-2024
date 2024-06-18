81d4538fc69f16c2bf20051b53522e30e2cf4926
DonghyoHong <gogoehdgy@naver.com>
2024-06-18 13:18:30 +0900

    # Options Api / Compostion Api (<script setup lang="ts">

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,29 @@
 <template>
   <div>
-    <ChildComponent
-        v-bind:sendProps1="title"
-        v-bind:sendProps2="year"
-        :sendProps3="obj"
-    ></ChildComponent>
+    부모 컴포넌트 Layout
+    <ChildComponent @send-event="parentEvent"></ChildComponent>
   </div>
 </template>
 
-<script setup lang="ts">
-//composition APi형태
+<script>
 import ChildComponent from "@/components/ChildComponent.vue";
-import {reactive, ref} from "vue";
 
-interface Obj {
-  id: Number,
-  name: String
-}
-
-const title = ref<string>("부모 컴포넌트에서 선언된 데이터입니다.");
-const year = ref<number>(2024);
-const obj = reactive<Obj>({
-  id: 2024
-  , name: "John"
-});
-
-// Opntion API 형태
-/*export default {
-// setup(){
-  data() {
-    return {
-      title: "부모 컴포넌트에서 선언된 데이터입니다."
-      , year: 2024
-      , obj: {
-        id: 2024
-        , name: "John"
-      }
-    }
-  }
-  , components: {
+export default {
+  components: {
     ChildComponent
   }
+  , methods: {
+    parentEvent(event) {
+      console.log(event);
+    }
+  }
+}
+
+//composition APi형태
+/*
+
+const parentEvent = (event: string) => {
+  console.log(event);
 }*/
 </script>
 
